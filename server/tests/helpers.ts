9d5494f7d45bb766beda1373f7d8ba0b94e45b7d
Iliya Idakiev <ilia.idakiev@gmail.com>
2023-12-26 09:18:02 +0200

    removed logging

--- server/tests/helpers.ts
+++ server/tests/helpers.ts
@@ -41,12 +41,6 @@ export const instance = axios.create({
 
 instance.interceptors.request.use(
   function (config) {
-    // console.log("Sending request", config);
-    fs.appendFile(
-      "/Users/iliaidakiev/Development/ahaplay-rework/server/output.txt",
-      `${JSON.stringify(config.data)}\n\n`,
-      () => {}
-    );
     return config;
   },
   function (error) {
