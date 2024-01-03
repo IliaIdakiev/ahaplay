8965ae161aa515cc16420ddbcaf1e5ffbb48610a
Iliya Idakiev <ilia.idakiev@gmail.com>
2024-01-03 22:29:14 +0200

    added activity associations

--- server/src/apollo/resources/activity.ts
+++ server/src/apollo/resources/activity.ts
@@ -11,6 +11,67 @@ export const activityTypeDefs = gql`
     Benchmark
   }
 
+  type Answer {
+    id: String!
+    create_date: Date!
+    update_date: Date!
+    text: String!
+    explanation_text: String
+    points: Int
+    activity_id: String
+  }
+
+  type Assignment {
+    duration: Int
+    text: String!
+    video: String!
+    activity_id: String!
+    conceptualization_id: String
+  }
+
+  type Benchmark {
+    baseline: String!
+    g_duration: Int!
+    i_duration: Int!
+    reference: String!
+    activity_id: String!
+    conceptualization_id: String!
+  }
+
+  type Concept {
+    id: String!
+    create_date: Date!
+    update_date: Date!
+    name: String!
+    text: String!
+    sequence_number: Int!
+    activity_id: String
+  }
+
+  type Conceptualization {
+    g_duration: Int!
+    i_duration: Int!
+    instructions: String!
+    activity_id: String!
+    concept: String!
+  }
+
+  type Question {
+    text: String!
+    g_duration: Int!
+    i_duration: Int!
+    activity_id: String!
+    assignment_id: String
+    theory_id: String
+  }
+
+  type Theory {
+    duration: Int!
+    video: String!
+    activity_id: String!
+    conceptualization_id: String
+  }
+
   type Activity {
     description: String
     sequence_number: Int
@@ -18,6 +79,13 @@ export const activityTypeDefs = gql`
     type: ActivityType
 
     workshop: Workshop
+    answers: [Answer]
+    assignment: Assignment
+    benchmark: Benchmark
+    concepts: [Concept]
+    conceptualization: Conceptualization
+    question: Question
+    theory: Theory
   }
 `;
 
